ed5cbc4b4a8207c64831e6c7d2c2a93c869da93b
darlenesterling <darlenesterling@C4Qs-MacBook-Pro-7.local>
2016-10-05 12:55:50 -0400

    updated home and data

--- src/data.js
+++ src/data.js
@@ -8,18 +8,18 @@ let data = {
       {animal:'walrus', adj:'funny', title:'Ride Sweetly', price:'$1,000,000', img:'./images1/walrus_funny3.png'},
       {animal:'walrus', adj:'weird', title:'Walrus Burger', price:'$450,000', img:'./images1/walrusWeird1.jpg'},
       {animal:'walrus', adj:'weird', title:'Lost', price:'$125,000', img:'./images1/walrusWeird2.jpg'},
-      {animal:'walrus', adj:'weird', title:'How Do You Do?', price:'', img:'./images1/walrusWeird3.jpg'},
+      {animal:'walrus', adj:'weird', title:'How Do You Do?', price:'$550,000', img:'./images1/walrusWeird3.jpg'},
 
       {animal:'dragon', adj:'eerie', title:'Borchanchi', price:'$150,000', img:'./images1/eerieDragon1.jpg'},
       {animal:'dragon', adj:'eerie', title:"Hell's Fury", price:'$375,000', img:'./images1/eerieDragon2.jpg'},
-      {animal:'dragon', adj:'eerie', title:'Demon Skull', price:'$500,000', img:'./images1/eerieDragon3.jpg'},
+      {animal:'dragon', adj:'eerie', title:'Demon Skull', price:'$525,000', img:'./images1/eerieDragon3.jpg'},
       {animal:'dragon', adj:'funny', title:'Tarut', price:'$1,000,000', img:'./images1/funnyDragon1.jpg'},
       {animal:'dragon', adj:'funny', title:'Lady "Huh"', price:'$800,000', img:'./images1/funnyDragon2.jpg'},
       {animal:'dragon', adj:'funny', title:"Putla's Fun Dining", price:'$250,000', img:'./images1/funnyDragon3.jpg'},
 
       {animal:'dragon', adj:'weird', title:'When You Sleep', price:'$400,000', img:'./images1/weirdDragon1.jpg'},
       {animal:'dragon', adj:'weird', title:'Percy the Dragon', price:'$150,000', img:'./images1/weirdDragon2.jpg'},
-      {animal:'dragon', adj:'weird', title:'Palandraba', price:'', img:'./images1/weirdDragon3.jpg'},
+      {animal:'dragon', adj:'weird', title:'Palandraba', price:'$350,000', img:'./images1/weirdDragon3.jpg'},
 
       {animal:'cat', adj:'eerie', title:'We All See', price:'$1,750,000', img:'./images1/eeriecat1.jpg'},
       {animal:'cat', adj:'eerie', title:"We're All Mad Here", price:'$145,000', img:'./images1/eeriecat2.jpg'},
@@ -44,9 +44,9 @@ let data = {
 
       {animal:'giraffe', adj:'eerie', title:"Death's Revenge", price:'$100,000', img:'./images1/eerieGiraffe1.jpg'},
       {animal:'giraffe', adj:'eerie', title:'Bug-ography', price:'$575,000', img:'./images1/eerieGiraffe2.jpg'},
-      {animal:'giraffe', adj:'eerie', title:'Fallen Fire', price:'3,500,000', img:'./images1/eerieGiraffe3.jpg'},
+      {animal:'giraffe', adj:'eerie', title:'Fallen Fire', price:'$3,500,000', img:'./images1/eerieGiraffe3.jpg'},
       {animal:'giraffe', adj:'funny', title:'Shall I Dance', price:'$250,000', img:'./images1/funnyGiraffe1.jpg'},
-      {animal:'giraffe', adj:'funny', title:'First Class', price:'100,000', img:'./images1/funnyGiraffe2.jpg'},
+      {animal:'giraffe', adj:'funny', title:'First Class', price:'$150,000', img:'./images1/funnyGiraffe2.jpg'},
       {animal:'giraffe', adj:'funny', title:"Who's Timmy", price:'$450,000', img:'./images1/funnyGiraffe3.png'},
       {animal:'giraffe', adj:'weird', title:'The Abyss', price:'$900,000', img:'./images1/weirdGiraffe1.jpg'},
       {animal:'giraffe', adj:'weird', title:'The Walk', price:'$350,000', img:'./images1/weirdGiraffe2.jpg'},
--- src/home.js
+++ src/home.js
@@ -1,5 +1,7 @@
-import React from 'react'
-import 'bootstrap/dist/css/bootstrap.css';
+import React from 'react';
+import ReactDOM from 'react-dom';import 'bootstrap/dist/css/bootstrap.css';
+import $ from 'jquery';
+
 
 
 var HomePage = React.createClass({
@@ -17,30 +19,32 @@ var Carousel = React.createClass({
   render: function(){
     return(
       <div>
-        <div id="myCarousel" className="carousel slide" data-ride="carousel">
-          {/* Indicators */}
+          <div id="myCarousel" className="carousel slide" data-ride="carousel">
+
             <ol className="carousel-indicators">
               <li data-target="#myCarousel" data-slide-to="0" className="active"></li>
               <li data-target="#myCarousel" data-slide-to="1"></li>
               <li data-target="#myCarousel" data-slide-to="2"></li>
             </ol>
+          </div>
 
-          {/*} Wrapper for slides */}
+
           <div className="carousel-inner" role="listbox">
               <div className="item active">
-                <img src="img_chania.jpg" alt="Chania" />
+                <img src={require("./images1/slidepic1.jpg")} alt="museum photo" />
               </div>
 
               <div className="item">
-                <img src="img_chania2.jpg" alt="Chania" />
+                <img src={require("./images1/slidepic2.jpg")} alt="walrus and unicorn art" />
               </div>
 
-              <div className="item">
-                <img src="img_flower.jpg" alt="Flower" />
+              <div className="item active">
+                <img src={require("./images1/slidepic3.jpg")} alt="art gallery view" />
               </div>
           </div>
 
-          {/* Left and right controls */}
+
+          <div>
               <a className="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
                 <span className="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
                 <span className="sr-only">Previous</span>
@@ -49,52 +53,52 @@ var Carousel = React.createClass({
                 <span className="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
                 <span className="sr-only">Next</span>
               </a>
-        </div>
+          </div>
 
       </div>
 
     )
   }
 })
-
+//This is the information under the image slider on the introductory page.
+//It holds information like museum hours, pricing etc.
 var PageInfo = React.createClass({
   render: function(){
     return(
+
       <div>
-        <div className="row">
-          <div id="myCarousel" className="carousel slide" data-ride="carousel">
-            {/* Indicators */}
-            <ol className="carousel-indicators">
-              <li data-target="#myCarousel" data-slide-to="0" className="active"></li>
-              <li data-target="#myCarousel" data-slide-to="1"></li>
-              <li data-target="#myCarousel" data-slide-to="2"></li>
-            </ol>
-
-
-        {/*} Wrapper for slides */}
-          <div className="carousel-inner" role="listbox">
-            <div className="item active">
-              <img src="img_chania.jpg" alt="Chania"/>
+        <div class="row">
+            <div class="col-md-8">
+              <h4>News</h4>
+              <p>Explore "AAA's" Unicorn Art</p>
             </div>
 
-            <div className="item">
-              <img src="img_chania2.jpg" alt="Chania"/>
+            <div class="col-md-4">
+              <h4>Our Sponsors:</h4>
+              <img src="http://http://2.bp.blogspot.com/-RMrynVlRuZc/TqI2cG9BWnI/AAAAAAAAAvM/TV-wIxpEOY8/s1600/ECF.png" />,
             </div>
-
-            <div className="item">
-              <img src="img_flower.jpg" alt="Flower"/>
-            </div>
-          </div>
         </div>
 
-        <div className="row">
-          <div className="col-md-4">.col-md-4</div>
-          <div className="col-md-4">.col-md-4</div>
-          <div className="col-md-4">.col-md-4</div>
-        </div>
-      </div>
+        <div class="row">
+            <div class="col-md-4">
+              <h4>Membership</h4>
+              <p>Special Rates & Benefits for Artists</p>
+            </div>
+
+            <div class="col-md-4">
+              <h4>Don&apos;t Miss:</h4>
+              <p>See Fisherman&apos;s Cry Artist Cortku</p>
+            </div>
+
+            <div class="col-md-4">
+              <h4>Program Calendar</h4>
+              <h5>October 18</h5>
+              <h5>Dragon Art Fair</h5>
+              <p>See the eeriest of eerie here</p>
+              <h6>7:00 pm</h6>
+            </div>
+         </div>
     </div>
-
     )
   }
 })
