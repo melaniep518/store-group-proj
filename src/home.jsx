daebbc133cea01d65fc08d7a3b27c609e3662028
darlenesterling <darlenesterling@C4Qs-MacBook-Pro-7.local>
2016-10-06 16:20:40 -0400

    update on home.js

--- src/home.jsx
+++ src/home.jsx
@@ -1,93 +1,92 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import $ from 'jquery';
+import Slider from 'react-slick';
+require ('./home.css')
 import 'bootstrap/dist/css/bootstrap.css';
-require('./index.css');
+
 
 
 var Home = React.createClass({
   render: function(){
     return(
       <div>
-        <Carousel />
+        <SimpleSlider />
         <PageInfo />
       </div>
     )
   }
 })
 
-var Carousel = React.createClass({
-  render: function(){
-    return(
-      <div id="carousel-example-generic" className="carousel slide" data-ride="carousel">
-        <ol className="carousel-indicators">
-          <li data-target="#carousel-example-generic" data-slide-to="0" className="active"></li>
-          <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-          <li data-target="#carousel-example-generic" data-slide-to="2"></li>
-        </ol>
-        <div className="carousel-inner" role="listbox">
-          <div className="carousel-item active">
-          <img src="http://i63.tinypic.com/2mngp3t.jpg" alt="museum" height="500" width="1107" />
-          </div>
-          <div className="carousel-item">
-            <img src="http://i66.tinypic.com/ol00nr.jpg" alt="art" height="500" width="1107"  />
-          </div>
-          <div className="carousel-item">
-            <img src="http://i67.tinypic.com/6fwks8.jpg" alt="gallery" height="500" width="1107" />
-          </div>
-        </div>
-        <a className="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
-          <span className="icon-prev" aria-hidden="true"></span>
-          <span className="sr-only">Previous</span>
-        </a>
-        <a className="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
-          <span className="icon-next" aria-hidden="true"></span>
-          <span className="sr-only">Next</span>
-        </a>
-      </div>
+
+var SimpleSlider = React.createClass({
+  render: function () {
+    var settings = {
+      dots: false,
+      infinite: true,
+      speed: 1500,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 1500
+    };
+    return (
+      <Slider {...settings}>
+        <div><img src="http://i63.tinypic.com/2mngp3t.jpg" alt="museum" height="500" width="1107" /></div>
+        <div><img src="http://i66.tinypic.com/ol00nr.jpg" alt="art" height="500" width="1107"  /></div>
+        <div><img src="http://i67.tinypic.com/6fwks8.jpg" alt="gallery" height="500" width="1107" /></div>
+      </Slider>
     )
   }
-})
+});
+
 //This is the information under the image slider on the introductory page.
 //It holds information like museum hours, pricing etc.
 var PageInfo = React.createClass({
-  render: function(){
-    return(
-      <div>
-        <div class="container">
-            <div class="row">
-              <div class=".col-{400}">
-                <h4>News</h4>
-                <p>Explore "AAA's" Unicorn Art</p>
+  render: function() {
+    return (
+      <div className="bottom-boxes">
+          <div className="info-box" width="100">
+              <div className="info">
+                <h6 font="bold">NEWS</h6>
+                <h5>Explore AAA's Unicorn Art</h5>
               </div>
 
-              <div class="col-xs">
-                <h4>Our Sponsors:</h4>
-                <img src="http://2.bp.blogspot.com/-RMrynVlRuZc/TqI2cG9BWnI/AAAAAAAAAvM/TV-wIxpEOY8/s1600/ECF.png" height="100" width="150" />
-              </div>
-            </div>
 
-            <div class="row">
-              <div class="col-xs">
-                <h4>Membership</h4>
-                <p>Special Rates & Benefits for Artists</p>
+              <div className="info dont-miss">
+                <h6>DON'T MISS</h6>
+                <h5>See Fisherman's Cry By: Artist Cortku</h5>
               </div>
-              <div class="col-xs">
-                <h4>Don&apos;t Miss:</h4>
-                <p>See Fisherman&apos;s Cry Artist Cortku</p>
+          </div>
+
+          <div className="info-page" width="100">
+              <div className="info">
+                <h6>OUR SPONSORS</h6>
+                <img src="http://www.ecf.net/site/wp-content/themes/ecf/img/exceptional-childrens-foundation.png" height="75" width="125" />
+                <img src="https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcT5lHFHfe-mShN7mQmtriJ5ivmnOk9nrNTfUOwiMsRXUWkdl4kj" height="75" width="125" />
               </div>
-              <div class="col-xs">
-                <h4>Program Calendar</h4>
-                <h5>October 18</h5>
+
+              <div className="info">
+                <h6>TODAY'S HOURS</h6>
+                <h5>10:00 a.m - 6:00 p.m.</h5>
+              </div>
+
+              <div className="info">
+                <h6>PROGRAM CALENDAR</h6>
+                <h6>10/18</h6>
                 <h5>Dragon Art Fair</h5>
-                <p>See the eeriest of eerie here</p>
-                <h6>7:00 pm</h6>
+                <h6>See the Eeriest of Eerie Art here</h6>
+                <h5>7:00 p.m.</h5>
               </div>
-            </div>
+          </div>
         </div>
-      </div>
-    )
-  }
-})
+
+
+
+
+
+      )
+    }
+});
 
 export default Home;
